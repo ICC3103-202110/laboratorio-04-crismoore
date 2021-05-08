ff7c2daabb5fa1e4d00ac70a429335f816ffae5a
Cristobal <cjmoore@miuandes.cl>
2021-05-08 17:51:45 -0400

    creacion de functionview(counter)

--- counter.js
+++ counter.js
@@ -1,4 +1,32 @@
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-console.log("(+)   (-)")
-console.log("(q) for quit")
+const functionview = (operation,counter)=> {
+    if (operation === '+'){
+        return (counter=counter+1);
+    }
+    else if (operation === '-') {
+        return (counter=counter-1);
+    }
+    else if (operation === 'q') {
+        return (false);
+    }
+}
+
+counter = 0
+while (true){
+
+    console.log(counter)
+    console.log("(+)   (-)")    
+    console.log("(q) for quit")
+
+
+    readline.question("What do you want to do? ", operation => {
+        console.log(functionview(operation, counter))
+
+        readline.close()
+    })
+}
